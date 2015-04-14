6059bb2112bd01c736b9e4dbd325a3be2cdb2a86
Klin <lj2lin@qq.com>
2015-04-14 23:38:17 +0800

    fix folder exists cause crash; fix header cause response can't be recongnize

--- downloadHtml.py
+++ downloadHtml.py
@@ -42,18 +42,13 @@ def downloadHtml(urlSuf, path):
     url = getCompleteUrl(urlSuf)
     print 'url: %s' % url
     req_header = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.11 (KHTML, like Gecko) '
-                                    'Chrome/23.0.1271.64 Safari/537.11',
-                  'Accept': 'text/html;q=0.9,*/*;q=0.8',
-                  'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
-                  'Accept-Encoding': 'gzip',
-                  'Connection': 'close',
-                  'Referer': None  # if can't work replace this as url Host
+                                    'Chrome/23.0.1271.64 Safari/537.11'
                   }
-    req_timeout = 10
+    req_timeout = 20
     req = urllib2.Request(url, None, req_header)
     response = urllib2.urlopen(req, None, req_timeout)
     html = response.read()
-    #print html
+    # print html
 
     # write html file to local
     fileName = ''
@@ -66,12 +61,13 @@ def downloadHtml(urlSuf, path):
     htmlFile.close()
 
     # create folder
-    os.mkdir(path + '/' + fileName.split('.')[0])
+    if not os.exists(path + '/' + fileName.split('.')[0]):
+        os.mkdir(path + '/' + fileName.split('.')[0])
 
     return fileName
 
 
-#
+# parse Parameters
 #
 #
 def parseOpt():
@@ -91,7 +87,7 @@ def parseOpt():
 
     return url, path
 
-# test function
+# main function
 #
 #
 def main(url, path):
